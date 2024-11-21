ba50ce24688ad5ba482f9e1e998aa72c5019bc1b
wenqingjie <wqjuser@gmail.com>
2024-11-21 15:19:02 +0800

    refactor(macos): 重构 macOS 插件代码
    - 删除了 MainFlutterWindow.swift 文件
    - 更新了 universal_code_viewer.podspec 文件
    - 优化了 UniversalCodeViewerPlugin.swift 文件中的代码

--- macos/Classes/UniversalCodeViewerPlugin.swift
+++ macos/Classes/UniversalCodeViewerPlugin.swift
@@ -2,21 +2,18 @@ import Cocoa
 import FlutterMacOS
 
 public class UniversalCodeViewerPlugin: NSObject, FlutterPlugin {
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "universal_code_viewer", binaryMessenger: registrar.messenger)
-        let instance = UniversalCodeViewerPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
-    }
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "universal_code_viewer", binaryMessenger: registrar.messenger)
+    let instance = UniversalCodeViewerPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
 
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch call.method {
-        case "getPlatformVersion":
-            // 获取 macOS 系统版本
-            let osVersion = ProcessInfo.processInfo.operatingSystemVersion
-            let versionString = "\(osVersion.majorVersion).\(osVersion.minorVersion).\(osVersion.patchVersion)"
-            result("macOS " + versionString)
-        default:
-            result(FlutterMethodNotImplemented)
-        }
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "getPlatformVersion":
+      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+    default:
+      result(FlutterMethodNotImplemented)
     }
+  }
 }
--- macos/universal_code_viewer.podspec
+++ macos/universal_code_viewer.podspec
@@ -1,12 +1,23 @@
+#
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html.
+# Run `pod lib lint universal_code_viewer.podspec` to validate before publishing.
+#
 Pod::Spec.new do |s|
   s.name             = 'universal_code_viewer'
-  s.version          = '1.0.3+1'
-  s.summary          = 'A universal code viewer plugin for macOS.'
-  s.description      = 'This plugin provides code viewing capabilities for macOS.'
-  s.homepage         = 'https://example.com'
-  s.license          = { :type => 'MIT', :file => '../LICENSE' }
-  s.author           = { 'Your Name' => 'email@example.com' }
+  s.version          = '0.0.1'
+  s.summary          = 'A new Flutter plugin project.'
+  s.description      = <<-DESC
+A new Flutter plugin project.
+                       DESC
+  s.homepage         = 'http://example.com'
+  s.license          = { :file => '../LICENSE' }
+  s.author           = { 'Your Company' => 'email@example.com' }
+
   s.source           = { :path => '.' }
-  s.platform         = :osx, '10.15'
-  s.source_files     = 'Classes/**/*'
+  s.source_files = 'Classes/**/*'
+  s.dependency 'FlutterMacOS'
+
+  s.platform = :osx, '10.11'
+  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
+  s.swift_version = '5.0'
 end
